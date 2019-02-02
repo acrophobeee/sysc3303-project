d475a822b3a299e0a32c7ad3b9c009caa3cf887e
YourNeptune <lixuanluo@icloud.com>
2019-02-02 18:29:10 -0500

    update java doc.

--- ElevatorSubsystem/src/elevatorSubsystem/Elevator.java
+++ ElevatorSubsystem/src/elevatorSubsystem/Elevator.java
@@ -14,18 +14,35 @@ public class Elevator {
 		this.elenumber = 1;
 	}
 
+	/**
+	 * @desc get elevator state in string
+	 * @return elevator state in string
+	 * */
 	public String getstate() {
 		return e.toString();
 	}
-
+	
+	/**
+	 * @desc get elevator state
+	 * @return elevator state
+	 * */
 	public Elevatorstate get() {
 		return e;
 	}
 
+	/**
+	 * @desc set elevator state
+	 * 
+	 * */
 	public void setstate(Elevatorstate state) {
 		e = state;
 	}
 
+	
+	/**
+	 * @desc change elevator mode
+	 *
+	 * */
 	public void changemode() {
 		if (floor.get(0)!=currentfloor) {
 		    if (floor.get(0) > currentfloor) {
@@ -53,21 +70,43 @@ public class Elevator {
 			    }
      	
 }	
+	
+	/**
+	 * @desc add the request into an arraylist
+	 *
+	 * */
     public void add(int requestfloor) {
        floor.add(requestfloor);	
     }
+    
+    /**
+	 * @desc increase floor number
+	 * */
 	public void incrasefloor() {
 		currentfloor++;
 	}
 
+	
+	/**
+	 * @desc decrease floor number
+	 * */
 	public void decreasefloor() {
 		currentfloor--;
 	}
 
+	/**
+	 * @desc get current floor number
+	 * @return number of current floor
+	 * */
 	public int getCurrentfloor() {
 		return currentfloor;
 	}
 
+	
+	/**
+	 * @desc get elevator number
+	 * @return elevator number
+	 * */
 	public int getElenumber() {
 		return elenumber;
 	}
