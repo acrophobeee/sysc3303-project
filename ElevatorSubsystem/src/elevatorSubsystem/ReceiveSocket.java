6b6a8480d1b2e2e132392e445ed91148531cec3b
nickc <nickc@172.17.133.116>
2019-02-10 18:39:15 -0500

    update

---
+++ ElevatorSubsystem/src/elevatorSubsystem/ReceiveSocket.java
@@ -0,0 +1,60 @@
+package elevatorSubsystem;
+
+import java.io.IOException;
+import java.net.*;
+
+public class ReceiveSocket {
+  private ElevatorSubsystem system;
+  DatagramSocket receiveSocket;
+  DatagramPacket sendPacket;
+  
+  public ReceiveSocket(ElevatorSubsystem system) {
+		try {
+			// Construct a datagram socket and bind it to any available
+			// port on the local host machine. This socket will be used to
+			// send UDP Datagram packets.
+			receiveSocket = new DatagramSocket(69);
+            this.system = system;
+			// to test socket timeout (2 seconds)
+			// receiveSocket.setSoTimeout(2000);
+		} catch (SocketException se) {
+			se.printStackTrace();
+			System.exit(1);
+		}
+	}
+  public void receive() throws Exception {
+		while (true) {
+			byte data[] = new byte[6];
+			sendPacket = new DatagramPacket(data, data.length);
+			System.out.println("Server: Waiting for Packet.\n");
+
+			// Block until a datagram packet is received from receiveSocket.
+			try {
+				System.out.println("Waiting..."); // so we know we're waiting
+				receiveSocket.receive(sendPacket);
+			} catch (IOException e) {
+				System.out.print("IO Exception: likely:");
+				System.out.println("Receive Socket Timed Out.\n" + e);
+				e.printStackTrace();
+				System.exit(1);
+			}
+		    	
+			// Process the received datagram.
+			System.out.println("Server: Packet received:");
+			System.out.println("From host: " + sendPacket.getAddress());
+			System.out.println("Host port: " + sendPacket.getPort());
+			int len = sendPacket.getLength();
+			System.out.println("Length: " + len);
+			System.out.print("Containing: ");
+          
+			system.put(sendPacket);
+			// Slow things down (wait 0.5 seconds)
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+  }
+}
