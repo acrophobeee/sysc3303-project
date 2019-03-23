9d741bff564204407e52fbba9d7cb00b3719667a
xinyuchen <xinyuchen@cb5109-28.labs.sce.carleton.ca>
2019-03-23 18:12:47 -0400

    add gui

---
+++ ElevatorSubsystem/src/Gui.java
@@ -0,0 +1,15 @@
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.*;
+
+
+public class Gui extends JFrame{
+
+	
+}
