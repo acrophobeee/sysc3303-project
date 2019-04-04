ddb21119c4a5e9490db3a493fbe4b0ce775b0bb0
unknown <xinruili3@cmail.carleton.ca>
2019-04-04 11:35:43 -0400

    add elevator status

---
+++ ElevatorSubsystem/src/DoorBlock.java
@@ -0,0 +1,31 @@
+
+public class DoorBlock implements Elevatorstate {
+
+	@Override
+	public void up() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void down() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void rest() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void opendoor() {
+		System.out.println("doorblock");
+
+	}
+
+	public String toString() {
+		return "doorblock";
+	}
+}
